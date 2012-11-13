fa3c7f21aefe9c204c9c42d82879b875b805a816
Vishal Sunak <vishalsunak@gmail.com>
2012-11-13 13:27:25 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ffl::Application.config.secret_token = '98c262e0af745cbdfe85d540e815ea87ee621ee9143dfefc73322149396ef42383f6a467e749ca75f73bee436de259e16069c0cbb915093848fa8761e6a83795'
